9f75077110dcb0de0f8b04e2b313569955253f5e
zxperts <zxperts@hotmail.com>
2025-08-29 23:47:00 +0200

    Ajout de nouvelles équipes

--- src/app/models/team.model.ts
+++ src/app/models/team.model.ts
@@ -31,13 +31,48 @@ export const TEAMS: Team[] = [
       { name: 'Stiliano', type: 'milieu' },
       { name: 'Mandares', type: 'milieu' },
       { name: 'Naofelle', type: 'milieu' },
-      { name: 'Danielle', type: 'milieu' },
+      { name: 'Daniel', type: 'milieu' },
       { name: 'Arthur', type: 'milieu' }
     ]
   },
   {
     id: 3,
     name: 'U9 Stand. Flawinne FC',
+    players: [
+      { name: 'Mathieu', type: 'milieu' },
+      { name: 'Loris', type: 'milieu' },
+      { name: 'Hugo', type: 'milieu' },
+      { name: 'Sam', type: 'milieu' },
+      { name: 'Achille', type: 'milieu' },
+      { name: 'Dario', type: 'milieu' },
+      { name: 'Jules', type: 'milieu' },
+      { name: 'Gabin', type: 'milieu' },
+      { name: 'Georges', type: 'milieu' }
+    ]
+  },
+  {
+    id: 4,
+    name: 'U11B Stand. Flawinne FC',
+    players: [
+      { name: 'Melvin', type: 'milieu' },
+      { name: 'Kalvin', type: 'milieu' },
+      { name: 'Thibault', type: 'milieu' },
+      { name: 'Tom', type: 'milieu' },
+      { name: 'Maxim', type: 'milieu' },
+      { name: 'Ely', type: 'milieu' },
+      { name: 'Mathéo', type: 'milieu' },
+      { name: 'Elson', type: 'milieu' },
+      { name: 'Shanna', type: 'milieu' },
+      { name: 'Stiliano', type: 'milieu' },
+      { name: 'Mandares', type: 'milieu' },
+      { name: 'Naofelle', type: 'milieu' },
+      { name: 'Daniel', type: 'milieu' },
+      { name: 'Arthur', type: 'milieu' }
+    ]
+  },
+  {
+    id: 5,
+    name: 'U10A Stand. Flawinne FC',
     players: [
       { name: 'Mathieu', type: 'milieu' },
       { name: 'Loris', type: 'milieu' },
